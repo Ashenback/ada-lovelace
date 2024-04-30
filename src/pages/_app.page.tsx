a0a8a4f946681528428373af175d91e271e599b4
Joakim Askenbäck <joakim.askenback@gmail.com>
2024-04-30 09:31:31 +0000

    remove de-DE locale remove language selector set env variables

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -3,19 +3,17 @@ import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
 import { Urbanist } from 'next/font/google';
 import './utils/globals.css';
-import { useRouter } from 'next/router';
 
 import { Layout } from '@src/components/templates/layout';
 
 const urbanist = Urbanist({ subsets: ['latin'], variable: '--font-urbanist' });
 
 const App = ({ Component, pageProps }: AppProps) => {
-  const { locale } = useRouter();
   return (
     <ContentfulLivePreviewProvider
       enableInspectorMode={pageProps.previewActive}
       enableLiveUpdates={pageProps.previewActive}
-      locale={locale || 'en-US'}
+      locale="en-US"
     >
       <>
         <main className={`${urbanist.variable} font-sans`}>
